434173643b9f19ce2512d13125e8c627db4f1b0d
Marina <marina@Marinas-MacBook-Pro.local>
2022-08-03 17:52:13 +0300

    first commit

---
+++ app/src/main/java/com/maroieqrwlk/unpi/black/CNST.kt
@@ -0,0 +1,16 @@
+package com.maroieqrwlk.unpi.black
+
+object CNST {
+    const val AF_DEV_KEY = "E2YRizXDLX4RaJgd3GAUp"
+    const val jsoupCheck = "6g2d"
+    const val ONESIGNAL_APP_ID = "e6cf0e19-96d1-4e5d-bee7-b1c8074f5054"
+
+    var lru = "http://lavenderstar.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
---
+++ app/src/main/java/com/maroieqrwlk/unpi/white/Game.kt
@@ -0,0 +1,12 @@
+package com.maroieqrwlk.unpi.white
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maroieqrwlk.unpi.R
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
